e13591724c91aff76b4ea395fbd7da6befae3f0c
b3tameche <b3tameche@gmail.com>
2025-05-18 02:13:56 +0400

    prance for resolving, openapi-spec-validator for validating

--- api_scoring_app/infra/validators/prance_validator.py
+++ api_scoring_app/infra/validators/prance_validator.py
@@ -1,28 +1,43 @@
-import json
-
 from typing import Any
-from prance import ResolvingParser, ValidationError
+from prance.util.resolver import RefResolver
+from prance.util.formats import parse_spec
+from prance import _PLACEHOLDER_URL as PLACEHOLDER_URL
+
+from openapi_spec_validator import OpenAPIV31SpecValidator
 
 from api_scoring_app.core.types import ValidationResult
 
 class PranceValidator:
     def __init__(self, spec_string: str) -> None:
-        self._validator = ResolvingParser(
-            spec_string=spec_string,
-            backend='openapi-spec-validator',
-            lazy=True
+        self.spec_string = spec_string
+
+    def _resolve(self) -> dict[str, Any]:
+        """
+        Uses Prance just for resolving references.
+        """
+
+        specification = parse_spec(self.spec_string, PLACEHOLDER_URL)
+
+        resolver = RefResolver(
+            specification,
+            PLACEHOLDER_URL
         )
+        resolver.resolve_references()
+        return resolver.specs
 
     def validate(self) -> ValidationResult:
+        """
+        Uses `openapi-spec-validator` for validating the resolved spec.
+        """
+        
+        # Resolve references first
+        resolved_spec = self._resolve()
+
         result = ValidationResult()
+        result.set_specification(resolved_spec)
 
-        try:
-            self._validator.parse()
-            result.set_specification(self._validator.specification)
-            return result
-        except ValidationError as exc:
-            result.add_error(f"Prance validation error: {exc}")
-        except Exception as exc:
-            result.add_error(f"OpenAPI spec parsing error: {exc}")
-
+        validator = OpenAPIV31SpecValidator(resolved_spec)
+        for each in validator.iter_errors():
+            result.add_error(each)
+        
         return result
